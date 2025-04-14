8b25c3d1cdfb4083c419baf6e9f553e047db622f
iheb <iheb.bentaieb@supcom.tn>
2025-04-14 18:35:38 +0100

    final commit

---
+++ SSc/Sensory-scape-main/src/pages/App.jsx
@@ -0,0 +1,47 @@
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import { AuthProvider } from './AuthContext';
+import 'typeface-inter'; // Import Inter font
+import LandingPage from './LandingPage';
+import SignIn from './SignIn';
+import SignUp from './SignUp';
+import Shop from './Shop';
+import ForumPage from './ForumPage';
+import Layout from './appbefore';
+import Board from './Board';
+import Control from './ControlPanel';
+import Fiche from './Fiche';
+import Home from './Home';
+import Contact from './Contact';
+import About from './About';
+import Chat from './Chat'; // Import the Chat component
+
+function App() {
+  return (
+    <AuthProvider>
+      <Routes>
+        {/* Public Routes */}
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/forums" element={<ForumPage />} />
+        <Route path="/signin" element={<SignIn />} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/shop" element={<Shop />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/about" element={<About />} />
+
+        {/* Dashboard Routes */}
+        <Route path="/dashboard" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="control" element={<Control />} />
+          <Route path="board" element={<Board />} />
+          <Route path="patient/:patientId" element={<Fiche />} />
+        </Route>
+
+        {/* Chat Route */}
+        <Route path="/chat" element={<Chat />} />
+      </Routes>
+    </AuthProvider>
+  );
+}
+
+export default App;
